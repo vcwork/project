9a1467eadb182eb41eb74634f7466ee9786dc618
femorenoba <femorenoba@users.noreply.github.com>
2022-04-06 03:21:19 +0000

    deploy: 9c721cb1c973c55f43c3dadaddb61c30614579a8

--- sketches/rgb.js
+++ sketches/rgb.js
@@ -1,49 +1,37 @@
+// Variables
+var deg = 0;
+var sp;
+let cnv,g;
 
-    let angle = 0; //declare a variable for the initial angle
-    let currentAnglePercentage = 0;
+//Dibuja el canvas
+function setup() { 
+  cnv = createCanvas(400, 400);
+  cnv.mouseClicked(changeGray);
+  sp =2;
+  g=100;
+}
 
-    function getCurrentAngle() {
-        return map(currentAnglePercentage % 100, 0, 100, 0, 360);
-    }
+function draw() { 
+  background(g);
+  translate (200,200);
+  rotate (radians (deg));
+  ellipse(0,0,200,200);
+  
+  // Crea y rellena los arcos de color
+  fill('red');
+  arc(0, 0, 200, 200, 0, 180);
+  fill('blue');
+  arc(0, 0, 200, 200, 180, 0);
+  fill('green');
+  arc(0, 0, 200, 200, 0, 360);
+  deg+=sp;
+}
 
-    function setup(){
-        createCanvas(400, 400);
-        noStroke();
-    }
-    
-    function draw(){
-        currentAnglePercentage++;
-        background(255);
-        noStroke;
-        angleMode(DEGREES);
-        ellipseMode(CENTER);
-
-        let posX = 200; //change the x axis
-        let posY = 200; //change the y axis
-        let reSize = 200; //change the size
-        let rotationSpeed = 0; //change the rotation speed
-
-        let radius = 0;
-        let x = radius * cos(angle);
-        let y = radius * sin(angle);
-
-        const c1a1 = 0 + getCurrentAngle();
-        const c1a2 = 120 + getCurrentAngle();
-
-        const c2a1 = 120 + getCurrentAngle();
-        const c2a2 = 240 + getCurrentAngle();
-
-        const c3a1 = 240 + getCurrentAngle();
-        const c3a2 = 360 + getCurrentAngle();
-
-        fill('red');
-        arc(posX + x, posY + y, reSize, reSize, c1a1, c1a2);
-
-        fill('blue');
-        arc(posX + x, posY + y, reSize, reSize, c2a1, c2a2);
-
-        fill('green');
-        arc(posX + x, posY + y, reSize, reSize, c3a1, c3a2);
-
-        angle += rotationSpeed;
-    }
+// Esta funcion se activa cuando se da click en el canvas aumenta la velocidad
+function mouseClicked() {
+  sp = sp + 3;
+}
+// Esta funcion se activa cuando se da click en el canvas cambia el color en escala de grises
+function changeGray() {
+  g = random(0, 200);
+}
