eb805681edb09af05eb78db863369f4d553fc8bb
OscarAlejandroGomez <OscarAlejandroGomez@users.noreply.github.com>
2022-05-22 16:22:11 +0000

    deploy: 4bbbb6403a71ca511e1db30e6abdbbbdfb527e19

---
+++ sketches/Combinado.js
@@ -0,0 +1,380 @@
+/**
+ * Finger painting: A starter sketch demonstrating how to "finger paint" with Handsfree.js
+ *
+ * Step 1: Press the ▶ Play Button on the top left of the p5 editor (or load this sketch in a browser)
+ * Step 2: Click "Start Webcam" underneath the black canvas
+ * Step 3: Wait a few moments for everything to start (the computer vision models are large)
+ * Step 4: Pinch your fingers to paint 👌 The left index pointer (big black circle) is an eraser and the left pinky clears the whole screen
+ *
+ * ----------
+ *
+ * How it works (CTRL+F for "#n" to jump to that bit of code):
+ * -- Add Handsfree.js to index.html
+ * #1 Configure Handsfree.js
+ * #2 Detect when fingers are pinched and then paint there
+ * #3 Draw your hands landmarks onto the sketch
+ *
+ * ----------
+ *
+ * Docs: https://handsfree.js.org (old)
+ * Docs: https://handsfree.dev (newer but missing examples)
+ * GitHub: https://github.com/midiblocks/handsfree
+ * Twitter (me + handsfree.js): https://github.com/pixelfelt
+ * Twitter (just handsfree.js): https://github.com/handsfreejs
+ *
+ * ----------
+ *
+ * Ideas:
+ * - Experiment with different input methods (like pinching to cycle through colors). Gestures are coming soon: https://handsfree.js.org/gesture/
+ * - If you need 3D (but with a limit of 1 hand), see here: https://handsfree.js.org/ref/model/handpose.html
+ * - Try the "palm pointer" for a different approach to painting: https://handsfree.js.org/ref/plugin/palmPointers.html
+ * - This can support up to 4 hands: https://handsfree.js.org/ref/model/hands.html#with-config
+ */
+
+
+// This will contain all of our lines
+paint = []
+
+// Controles de las brocha
+let easycam;
+let state;
+
+let escorzo;
+let points;
+let record;
+
+let dx;
+let dy;
+
+
+let colorPi;
+let depth;
+let brush;
+
+
+
+// This is like pmouseX and pmouseY...but for every finger [pointer, middle, ring, pinky]
+let prevPointer = [
+  // Left hand
+  [{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}],
+  // Right hand
+  [{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}]
+]
+
+let posPointer = [
+  // Left hand
+  [{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}],
+  // Right hand
+  [{x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}]
+]
+
+let PrevpointerCopy = prevPointer;
+
+// Landmark indexes for fingertips [pointer, middle, ring, pinky]...these are the same for both hands
+let fingertips = [8, 12, 16, 20]
+
+
+/**
+ * Setup
+ * - Configure handsfree (set which models, plugins, and gestures you want to use)
+ * - Create start/stop buttons. It's nice to always ask user for permission to start webcam :)
+ */
+function setup () {
+  depth = 0;
+  record = true;
+  sketch = createCanvas(640, 480, WEBGL);
+
+
+  colorPi = createColorPicker('#ed225d');
+  colorPi.position(width - 70, 40);
+  
+  dx = sketch.width/2;
+  dy = sketch.height / 2;
+  points = []
+  
+  
+  // Colors for each fingertip
+  colorMap = [
+    // Left fingertips
+    [color(0, 0, 0), color(255, 0, 255), color(0, 0, 255), color(255, 255, 255)],
+    // Right fingertips
+    [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255), color(255, 255, 0)]
+  ]
+  
+  // #1 Turn on some models (hand tracking) and the show debugger
+  // @see https://handsfree.js.org/#quickstart-workflow
+  handsfree = new Handsfree({
+    showDebug: true, // Comment this out to hide the default webcam feed with landmarks
+    hands: true
+  })
+  handsfree.enablePlugins('browser')
+  handsfree.plugin.pinchScroll.disable()
+  
+  // Add webcam buttons under the canvas
+  // Handsfree.js comes with a bunch of classes to simplify hiding/showing things when things are loading
+  // @see https://handsfree.js.org/ref/util/classes.html#started-loading-and-stopped-states
+  buttonStart = createButton('Start Webcam')
+  buttonStart.class('handsfree-show-when-stopped')
+  buttonStart.class('handsfree-hide-when-loading')
+  buttonStart.mousePressed(() => handsfree.start())
+
+  // Create a "loading..." button
+  buttonLoading = createButton('...loading...')
+  buttonLoading.class('handsfree-show-when-loading')
+
+  // Create a stop button
+  buttonStop = createButton('Stop Webcam')
+  buttonStop.class('handsfree-show-when-started')
+  buttonStop.mousePressed(() => handsfree.stop())
+
+  // easycam stuff
+  let state = {
+    distance: 250,           // scalar
+    center: [0, 0, 0],       // vector
+    rotation: [0, 0, 0, 1],  // quaternion
+  };
+  easycam = createEasyCam();
+  easycam.state_reset = state;   // state to use on reset (double-click/tap)
+  easycam.setState(state, 2000); // now animate to that state
+  escorzo = true;
+  perspective();
+
+  brush = sphereBrush;
+}
+
+
+/**
+ * Main draw loop
+ */
+function draw () {
+  background(0)
+  fingerPaint()
+  mousePaint()
+  drawHands()
+
+  update();
+  background(120);
+  push();
+  strokeWeight(0.8);
+  stroke('magenta');
+  grid({ dotted: false });
+  pop();
+  axes();
+  
+  for (const point of points) {
+    push();
+    translate(point.worldPosition);
+    brush(point);
+    pop();
+  }
+
+
+
+  // Revisar la rotacion en x y y ,
+  handsfree.on('finger-pinched-0-0', () => {
+    // Display the x and y of the right pinky
+    if(handsfree.data.hands.curPinch[0][0].x != posPointer[0][0].x || handsfree.data.hands.curPinch[0][0].y != posPointer[0][0].y){
+      if(handsfree.data.hands.curPinch[0][0].x   > posPointer[0][0].x){
+        easycam.rotateY(0.1);
+        //console.log("derecha");
+      }if(handsfree.data.hands.curPinch[0][0].x  < posPointer[0][0].x){
+        easycam.rotateY(-0.1);
+        //console.log("izquierda");
+      }if(handsfree.data.hands.curPinch[0][0].y  >  posPointer[0][0].y){
+        easycam.rotateX(0.1);
+        //console.log("arriba");
+      }if(handsfree.data.hands.curPinch[0][0].y   < posPointer[0][0].y){
+        easycam.rotateX(-0.1);
+        //console.log("abajo");
+      } 
+    }
+    posPointer[0][0].x = handsfree.data.hands.curPinch[0][0].x;
+    posPointer[0][0].y = handsfree.data.hands.curPinch[0][0].y;
+  })
+
+ // revisar la rotacion en z
+  handsfree.on('finger-pinched-0-1', () => {
+    // Display the x and y of the right pinky
+    if(handsfree.data.hands.curPinch[0][1].y != posPointer[0][1].y){
+      if(handsfree.data.hands.curPinch[0][1].y > posPointer[0][1].y){
+        easycam.zoom(15);
+        //console.log("arriba");
+      }if(handsfree.data.hands.curPinch[0][1].y < posPointer[0][1].y){
+        easycam.zoom(-15);
+        //console.log("abajo");
+      } 
+    }
+    posPointer[0][1].y = handsfree.data.hands.curPinch[0][1].y;
+  })
+  
+
+}
+
+
+function update() {
+
+  speed = constrain((dx + dy) / (2 * (width - height)), 0, 1);
+
+  // Dibujar con un color
+  handsfree.on('finger-pinched-1-0', () => {
+    // Display the x and y of the right pinky
+    if(handsfree.data.hands.curPinch[1][0].x != posPointer[1][0].x || handsfree.data.hands.curPinch[1][0].y != posPointer[1][0].y){
+      if(handsfree.data.hands.curPinch[1][0].x   > posPointer[1][0].x){
+        dx -= 10 ; 
+      }if(handsfree.data.hands.curPinch[1][0].x  < posPointer[1][0].x){
+        dx += 10 ;
+      }if(handsfree.data.hands.curPinch[1][0].y  >  posPointer[1][0].y){
+        dy -= 10 ;
+      }if(handsfree.data.hands.curPinch[1][0].y   < posPointer[1][0].y){
+        dy += 10;
+      } 
+    }
+    console.log(dx, "  ", dy, " ", depth);
+    posPointer[1][0].x = handsfree.data.hands.curPinch[1][0].x;
+    posPointer[1][0].y = handsfree.data.hands.curPinch[1][0].y;
+  })
+
+  if(handsfree.data.hands && handsfree.data.hands.pinchState){
+    console.log("esta con estados");
+    if (handsfree.data.hands.pinchState[0][2] == 'held') {
+      console.log("esta grabando");
+            points.push({
+        worldPosition: treeLocation([dx, dy, 0.5], { from: 'SCREEN', to: 'WORLD' }),
+        color: colorPi.color(),
+        speed: speed
+      });
+    }
+  }
+
+}
+
+
+
+
+
+/**
+ * #2 Finger paint
+ * Since p5.js already has it's own loop, we just check the data directly
+ * @see https://handsfree.js.org/ref/plugin/pinchers.html
+ */
+// Whenever we pinch and move we'll store those points as a set of [x1, y1, handIndex, fingerIndex, size]
+function fingerPaint () {
+  let bounds = document.querySelector('canvas').getClientRects()[0]
+  const hands = handsfree.data?.hands
+
+  if (hands?.pinchState) {
+    hands.pinchState.forEach((hand, handIndex) => {
+      hand.forEach((state, finger) => {
+        if (hands.landmarks?.[handIndex]?.[fingertips[finger]]) {
+          let x = sketch.width - hands.landmarks[handIndex][fingertips[finger]].x * sketch.width
+          let y = hands.landmarks[handIndex][fingertips[finger]].y * sketch.height
+
+          // Start line on the spot that we pinched
+          if (state === 'start') {
+            prevPointer[handIndex][finger] = {x, y}
+
+          // Add a line to the paint array
+          } else if (state === 'held') {
+            paint.push([
+              prevPointer[handIndex][finger].x,
+              prevPointer[handIndex][finger].y,
+              x,
+              y,
+              colorMap[handIndex][finger]
+            ])
+          }
+
+          // Set the last position
+          prevPointer[handIndex][finger] = {x, y}          
+        }
+      })
+    })  
+  } 
+
+  // Clear everything if the left [0] pinky [3] is pinched
+  if (hands?.pinchState && hands.pinchState[0][3] === 'released') {
+    paint = []
+  }
+  
+  // Draw Paint
+  paint.forEach(p => {
+    push()
+    fill(255)
+    stroke(p[4])
+    strokeWeight(10)
+    line(p[0], p[1], p[2], p[3])
+    pop();
+  })
+}
+
+
+
+/**
+ * Draw the mouse
+ */
+function mousePaint () {
+  if (mouseIsPressed === true) {
+    fill(colorMap[1][0])
+    stroke(colorMap[1][0])
+    strokeWeight(10)
+    line(mouseX, mouseY, 0  , pmouseX, pmouseY, 0)
+  }
+}
+
+
+/**
+ * #3 Draw the hands into the P5 canvas
+ * @see https://handsfree.js.org/ref/model/hands.html#data
+ */
+function drawHands () {
+  const hands = handsfree.data?.hands
+  
+  // Bail if we don't have anything to draw
+  if (!hands?.landmarks) return
+  
+  // Draw keypoints
+  hands.landmarks.forEach((hand, handIndex) => {
+    hand.forEach((landmark, landmarkIndex) => {
+      // Set color
+      // @see https://handsfree.js.org/ref/model/hands.html#data
+      if (colorMap[handIndex]) {
+        switch (landmarkIndex) {
+          case 8: fill(colorMap[handIndex][0]); break
+          case 12: fill(colorMap[handIndex][1]); break
+          case 16: fill(colorMap[handIndex][2]); break
+          case 20: fill(colorMap[handIndex][3]); break
+          default:
+            fill(color(255, 255, 255))
+        }                
+      }
+      // Set stroke
+      if (handIndex === 0 && landmarkIndex === 8) {
+        stroke(color(255, 255, 255))
+        strokeWeight(5)
+        circleSize = 40
+      } else {
+        stroke(color(0, 0, 0))
+        strokeWeight(0)
+        circleSize = 10
+      }
+      
+      circle(
+        // Flip horizontally
+        sketch.width - landmark.x * sketch.width,
+        landmark.y * sketch.height,
+        circleSize
+      )
+    })
+  })
+
+}
+
+function sphereBrush(point) {
+  push();
+  noStroke();
+  // TODO parameterize sphere radius and / or
+  // alpha channel according to gesture speed
+  fill(point.color);
+  sphere(1);
+  pop();
+}
---
+++ sketches/sphereTest.js
@@ -0,0 +1,297 @@
+// Ray Casting
+// aruvham
+
+var worldMap = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,1],
+                [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+                [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+                [1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1],
+                [1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1,1,1,1],
+                [1,0,0,0,0,0,0,0,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1],
+                [1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1],
+                [1,0,0,1,1,1,0,0,0,1,1,1,0,1,1,1,1,1,1,0,1,1,1,0,0,0,1,1,1,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
+
+var mapWidth = worldMap[0].length, // in blocks
+    mapHeight = worldMap.length;   // in blocks
+
+var screenWidth = 320, // px
+    screenHeight = 240;// px
+
+var FOV = 80, // degrees
+    resolution = 1, // px
+    numberOfRays = screenWidth / resolution;
+
+function setup() {
+  createCanvas(640, 240);
+  minimap = new Minimap(0, 0, 10);
+  player = new Player(150, 170);
+}
+
+function draw() {
+  noStroke();
+  background(255);
+  fill(255);
+  rect(320,0,320,240);
+  fill(0);
+  rect(320,0,320,120);
+  fill(200);
+  rect(320,120,320,120);
+
+  minimap.draw();
+  castRays();
+  player.update();
+  player.draw();
+}
+
+function castRays() {
+  var angleStep = FOV / numberOfRays; // 60 / 80 = 0.75
+  var radianStep = (angleStep / 180) * PI // 0.013
+  var initialAngle = player.dir - (0.5 * FOV / 180) * PI;
+  stroke(0);
+
+  for(var i = 0; i < numberOfRays; i++) {
+
+    // make sure angle is between 0 and 2PI rads
+    var newAngle = initialAngle + (radianStep * i);
+    if(newAngle < 0) newAngle = (2 * PI) + newAngle;
+    var distance = castSingleRay(newAngle);
+    stroke("#FFF1A7");
+    line(player.x, player.y, player.x + distance[0] * cos(newAngle), player.y + distance[0] * sin(newAngle));
+
+    // render
+    var z = (distance[0]/10);
+    var z = z * cos(player.dir - newAngle);
+    var wallHeight = 240/z;
+    if(wallHeight > 240) wallHeight = 240;
+
+    fill("#01A1A1");
+    stroke("#01A1A1");
+    if(distance[1] == 0 || distance[1] == 1) {
+          fill("#016666");
+          stroke("#016666");
+    }
+    rect(320 + i * resolution, 120-(0.5 * wallHeight), resolution, wallHeight);
+  }
+}
+
+function castSingleRay(angle) {
+  // Moving right/left? up/down? Determined by
+  // which quadrant the angle is in
+  var right = angle > (3 * PI / 2) || (angle >= 0 && angle < PI / 2);
+  var up = angle > PI && angle < 2 * PI;
+
+  // HORIZONTAL WALL COLLISIONS
+  var hCol = horizontalCollision(up, angle);
+  var vCol = verticalCollision(right, angle);
+  return (hCol[0] < vCol[0]) ? hCol : vCol;
+}
+
+function horizontalCollision(up, angle) {
+  var x = floor(player.gridX);
+  var y = up ? floor(player.gridY) : floor(player.gridY) + 1;
+  var aY = y * minimap.scale;
+  var aX = player.x - (player.y - aY) / tan(angle);
+  var dX = minimap.scale / tan(angle);
+  var dY = minimap.scale;
+  var distance = [1000, 0]; // arbitrary large number
+  var i = 0;
+
+  // moving up
+  if(up) {
+    // while inside the map
+    while((aY - i * dY) >= minimap.y) {
+      // cordinate positions
+      var posX = aX - i * dX;
+      var posY = aY - i * dY;
+      // grid positions
+      var gX = floor(posX / minimap.scale);
+      var gY = (posY/ minimap.scale) - 1;
+
+      if((gX < mapWidth) && (gY >= 0) && worldMap[gY][gX] != 0) {
+        distance = [dist(player.x, player.y, posX, posY), 0];
+        break;
+      }
+      i++;
+    }
+    // moving down
+  } else {
+    while((aY + i * dY) <= minimap.y + mapHeight * minimap.scale) {
+      var posX = aX + i * dX;
+      var posY = aY + i * dY;
+      var gX = floor(posX / minimap.scale);
+      var gY = (posY)/ minimap.scale;
+
+      if((gX < mapWidth) && (gY >= 0) && worldMap[gY][gX] != 0) {
+        distance = [dist(player.x, player.y, posX, posY), 1];
+        break;
+      }
+      i++;
+    }
+  }
+  return distance;
+}
+
+function verticalCollision(right, angle) {
+  var x = right ? floor(player.gridX) + 1: floor(player.gridX);
+  var y = floor(player.gridY);
+  var aX = x * minimap.scale;
+  var aY = player.y + (aX - player.x) * tan(angle);
+  var dY = minimap.scale * tan(angle);
+  var dX = minimap.scale;
+
+  var distance = [1000, 0]; // arbitrary large number
+  var i = 0;
+  fill(255, 0, 255);
+
+  // moving right
+  if(right) {
+    // while inside the map
+    while((aX + i * dX) <= minimap.x + mapWidth * minimap.scale) {
+      // cordinate positions
+      var posX = aX + i * dX;
+      var posY = aY + i * dY;
+      // grid positions
+      var gX = posX / minimap.scale;
+      var gY = floor(posY/ minimap.scale);
+
+      if((gX < mapWidth) && (gY >= 0) && (gY < mapHeight) && worldMap[gY][gX] != 0) {
+
+        distance = [dist(player.x, player.y, posX, posY), 2];
+        break;
+      }
+      i++;
+    }
+  } else {
+    // while inside the map
+    while((aX - i * dX) >= minimap.x) {
+      // cordinate positions
+      var posX = aX - i * dX;
+      var posY = aY - i * dY;
+      // grid positions
+      var gX = (posX / minimap.scale) - 1;
+      var gY = floor(posY/ minimap.scale);
+
+      if((gX >= 0) && (gY >= 0) && (gY < mapHeight) && worldMap[gY][gX] != 0) {
+        distance = [dist(player.x, player.y, posX, posY), 3];
+        break;
+      }
+      i++;
+    }
+  }
+  return distance;
+}
+
+function Player(x, y) {
+  this.x = x + minimap.x;
+  this.y = y + minimap.y;
+  this.gridX = this.x / minimap.scale;
+  this.gridY = this.y / minimap.scale;
+  this.dir = 0; // angle
+  this.rot = 0;
+  this.speed = 0;
+  this.moveSpeed = 1;
+  this.rotSpeed = 2.5 * PI/180;
+  this.radius = 4;
+
+  this.draw = function() {
+    var squareSize = (2 * this.radius) * (2 * this.radius);
+    squareSize /= 2;
+    squareSize = sqrt(squareSize);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    rectMode(CENTER);
+    rect(this.x, this.y, squareSize, squareSize);
+    rectMode(CORNER);
+  }
+
+  this.update = function() {
+    // so player angle is always between 0 and 2PI rads
+    this.dir += (this.rot > 0) ? this.rot * this.rotSpeed : 2 * PI + this.rot * this.rotSpeed;
+    this.dir %= 2 * PI;
+    newX = this.x + this.speed * this.moveSpeed * cos(this.dir);
+    newY = this.y + this.speed * this.moveSpeed * sin(this.dir);
+
+    if(!this.collision(newX, newY)) {
+      this.x = newX;
+      this.y = newY;
+    }
+
+    this.gridX = this.x / minimap.scale;
+    this.gridY = this.y / minimap.scale;
+  }
+
+  this.collision = function(newX, newY) {
+    if(newX - this.radius < minimap.x ||
+       newX + this.radius > minimap.x + mapWidth * minimap.scale ||
+       newY - this.radius < minimap.y ||
+       newY + this.radius > minimap.y + mapHeight * minimap.scale) {
+      return true;
+    }
+    return worldMap[floor((newY - minimap.y) / minimap.scale)][floor((newX - minimap.x) / minimap.scale)] != 0;
+  }
+}
+
+function Minimap(x, y, scale) {
+  this.x = x;
+  this.y = y;
+  this.scale = scale; // in pixels
+
+  this.draw = function() {
+    fill(255);
+    noStroke();
+    rect(this.x, this.y, mapWidth * this.scale, mapHeight * this.scale);
+
+    fill(200);
+    for(y = 0; y < mapHeight; y++) {
+      for(x = 0; x < mapWidth; x++) {
+        if(worldMap[y][x] != 0) {
+          rect(this.x + x * this.scale, this.y + y * this.scale, this.scale, this.scale);
+        }
+      }
+    }
+  }
+
+  this.drawGrid = function() {
+    stroke(0);
+    for(x = 0; x <= mapWidth; x++) {
+      line(this.x + x * this.scale, this.y, this.x + x * this.scale, this.y + mapHeight * this.scale);
+    }
+    for(y = 0; y <= mapHeight; y++) {
+      line(this.x, this.y + y * this.scale, this.x + mapWidth * this.scale, this.y + y * this.scale);
+    }
+  }
+}
+
+function keyPressed() {
+  if(keyCode == 87) player.speed = 1;
+  if(keyCode == 83) player.speed = -1;
+  if(keyCode == 65) player.rot = -1;
+  if(keyCode == 68) player.rot = 1;
+}
+
+function keyReleased() {
+  if(keyCode == 87 || keyCode == 83) player.speed = 0;
+  if(keyCode == 65 || keyCode == 68) player.rot = 0;
+}
+
+/*
+window.addEventListener("keydown", function(e) {
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);*/
