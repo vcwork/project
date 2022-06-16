bca4c371e4b71b53e76b18a7508e5b553265b739
femorenoba <femorenoba@users.noreply.github.com>
2022-06-16 16:12:45 +0000

    deploy: 1008e46e01be1224073422d2bb23c4c67cbf4c99

---
+++ sketches/uv_triangle.js
@@ -0,0 +1,28 @@
+let uvShader;
+
+function preload() {
+  // Define geometry directly in clip space (i.e., matrices: Tree.NONE).
+  // Interpolate only texture coordinates (i.e., varyings: Tree.texcoords2).
+  // see: https://github.com/VisualComputing/p5.treegl#handling
+  uvShader = readShader('/project/sketches/shaders/uv.frag', { matrices: Tree.NONE, varyings: Tree.texcoords2 });
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(600, 450, WEBGL);
+  noStroke();
+  // see: https://p5js.org/reference/#/p5/shader
+  shader(uvShader);
+  // https://p5js.org/reference/#/p5/textureMode
+  // best and simplest is to just always used NORMAL
+  textureMode(NORMAL);
+}
+
+function draw() {
+  background(0);
+  // clip-space quad (i.e., both x and y vertex coordinates ∈ [-1..1])
+  // https://p5js.org/reference/#/p5/quad
+  // It's worth noting (not mentioned in the api docs) that the quad
+  // command also adds the texture coordinates to each of its vertices.
+  triangle(1,-1, 1,1, -1,1);
+}
