32b112a0e53f1de26aba9b5e90b31849bbee435c
ogomezs <ogomezs@unal.loc>
2022-06-22 10:52:42 -0500

    Cambios en el shader

--- content/sketches/mixing.js
+++ content/sketches/mixing.js
@@ -1,14 +1,20 @@
 let lumaShader;
 let img;
 let grey_scale;
+let colorBack;
+let r , g, b ;
 
 function preload() {
-  lumaShader = readShader('/project/sketches/shaders/mixing.frag', { varyings: Tree.texcoords2 });
+  lumaShader = readShader('/project/sketches/shaders/mixing.frag', { varyings: Tree.texcoords2});
   // image source: https://en.wikipedia.org/wiki/HSL_and_HSV#/media/File:Fire_breathing_2_Luc_Viatour.jpg
   img = loadImage('/project/sketches/fire.jpg');
 }
 
 function setup() {
+  r = 110;
+  g = 0;
+  b = 256;
+  colorBack = [r,g,b];
   createCanvas(700, 500, WEBGL);
   noStroke();
   textureMode(NORMAL);
@@ -16,11 +22,14 @@ function setup() {
   grey_scale = createCheckbox('luma', false);
   grey_scale.position(10, 10);
   grey_scale.style('color', 'white');
-  grey_scale.input(() => lumaShader.setUniform('grey_scale', grey_scale.checked()));
+  grey_scale.input(() => lumaShader.setUniform('background', colorBack));
   lumaShader.setUniform('texture', img);
 }
 
 function draw() {
-  background(255);
+  background(r,g,b);
   quad(-width / 2, -height / 2, width / 2, -height / 2, width / 2, height / 2, -width / 2, height / 2);
+  r = (r + 1) % 256;
+  g = (g + 1) % 256;
+  b = (g + 1) % 256;
 }
--- content/sketches/shaders/mixing.frag
+++ content/sketches/shaders/mixing.frag
@@ -3,18 +3,22 @@ precision mediump float;
 // uniforms are defined and sent by the sketch
 uniform bool grey_scale;
 uniform sampler2D texture;
+uniform vec3 background;
 
 // interpolated texcoord (same name and type as in vertex shader)
 varying vec2 texcoords2;
 
 // returns luma of given texel
-float luma(vec3 texel) {
-  return  texel.r * texel.g * texel.b;
+vec3 luma(vec3 texel) {
+  float v1 = mix(texcoords2.r, background[0], 0.2);
+  float v2 = mix(texcoords2.g, background[1], 0.6);
+  float v3 = mix(texcoords2.r, background[2], 0.3);
+  return vec3(v1,v2,v3);
 }
 
 void main() {
   // texture2D(texture, texcoords2) samples texture at texcoords2 
   // and returns the normalized texel color
   vec4 texel = texture2D(texture, texcoords2);
-  gl_FragColor = grey_scale ? vec4((vec3(luma(texel.rgb))), 1.0) : texel;
+  gl_FragColor = grey_scale ? vec4((luma(texel.rgb)), 1.0) : texel;
 }
